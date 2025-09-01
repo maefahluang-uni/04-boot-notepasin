35e18ac2381e58b94ae23096f3f926f47cf1952d
noppasin <6731503016@lamduan.mfu.ac.th>
2025-09-01 10:01:26 +0000

    Lab04_6731503016

--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -6,31 +6,39 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    public static Map<String, User> users = new HashMap<String, User>();
+    public static Map<String, User> users = new HashMap<>();
 
-   
-    public ResponseEntity<String> registerUser( User user) {
-      //TODO
-      return null;
+    @PostMapping
+    public ResponseEntity<String> registerUser(@RequestBody(required = false) User user) {
+        if (user == null || user.getUsername() == null || user.getUsername().isBlank()) {
+            return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
+        }
+        String username = user.getUsername();
+        if (users.containsKey(username)) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
+        }
+        users.put(username, user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
     }
 
+    @GetMapping
     public ResponseEntity<Collection<User>> list() {
-        //TODO
-        return null;
+        return new ResponseEntity<>(users.values(), HttpStatus.OK);
     }
 
-    public ResponseEntity<User> getUser(String username) {
-        //TODO
-        return null;
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User found = users.get(username);
+        if (found == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(found, HttpStatus.OK);
     }
-    
 }
+
